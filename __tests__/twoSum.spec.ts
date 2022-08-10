a80d953feb559bc4c330688d58fdc9f3141102dc
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2022-08-10 17:27:09 -0300

    WIP: backup

--- __tests__/twoSum.spec.ts
+++ __tests__/twoSum.spec.ts
@@ -1,16 +1,22 @@
-import twoSum from "@src/twoSum";
+import twoSum from "@src/TwoSum";
 
 const cases = [
-  [[2,7,11,15], 9, [0,1]],
-  [[3,2,4], 6, [1,2]],
-  [[3,3], 6, [0,1]]
-]
+  [[2, 7, 11, 15], 9, [0, 1]],
+  [[3, 2, 4], 6, [1, 2]],
+  [[3, 3], 6, [0, 1]],
+];
 describe("Two Sum Problem", () => {
-  it.concurrent.each(cases)(
-    "Given %p and %p as arguments, returns %p",         
-    (firstArg: Array<number>, secondArg: number, expectedResult: Array<number>) => {
-      const result = twoSum(firstArg, secondArg);
-      expect(result).toEqual(expectedResult);
-    }
-  )
+  describe("First Solution", () => {
+    it.concurrent.each(cases)(
+      "Given %p and %p as arguments, returns %p",
+      (
+        firstArg: Array<number>,
+        secondArg: number,
+        expectedResult: Array<number>
+      ) => {
+        const result = twoSum(firstArg, secondArg);
+        expect(result).toEqual(expectedResult);
+      }
+    );
+  });
 });
--- src/twoSum.ts
+++ src/twoSum.ts
@@ -1,17 +1,16 @@
-let verifySum = (nums: number[], index: number, target: number) => {
-  for (let i = index + 1; i< nums.length; i++) {
-      if (nums[i] + nums[index] === target) {
-          return [index, i]
-      }
+const verifySum = (nums: number[], index: number, target: number) => {
+  for (let i = index + 1; i < nums.length; i++) {
+    if (nums[i] + nums[index] === target) {
+      return [index, i];
+    }
   }
-  return []
-  
-}
-
-export default function(nums: number[], target: number) {
-  let output = []
-  for (let i = 0; i < nums.length; i++) {
-      output = verifySum(nums, i, target)
-      if (output.length) return output
-  }
+  return [];
 };
+
+export default function (nums: number[], target: number) {
+  let output = [];
+  for (let i = 0; i < nums.length; i++) {
+    output = verifySum(nums, i, target);
+    if (output.length) return output;
+  }
+}
