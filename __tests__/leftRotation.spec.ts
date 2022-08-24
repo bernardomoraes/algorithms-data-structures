3bc20ce620baacc0e7878c42a4e7be72ef95adcf
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2022-08-24 00:46:53 -0300

    Add rotate left algo

---
+++ __tests__/leftRotation.spec.ts
@@ -0,0 +1,13 @@
+import rotateLeft from "@src/leftRotation";
+
+describe("Left Rotation", () => {
+  it("should return propperly", () => {
+    const reponse = [5, 1, 2, 3, 4];
+    const arr = [1, 2, 3, 4, 5];
+    const rotAmount = 4;
+
+    const result = rotateLeft(rotAmount, arr, arr.length);
+
+    expect(result).toEqual(reponse);
+  });
+});
